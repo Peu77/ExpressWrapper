150e86941a9319c1cf75c1b5cef42eba05e0f08b
peu77 <emilebert@icloud.com>
2022-07-22 21:59:28 +0200

    first commit

---
+++ Controller.ts
@@ -0,0 +1,54 @@
+import {RequestListener, Service, ServiceResponseData} from "./Service";
+import {Express} from "express";
+
+export type Controller = {
+    prefix: string,
+    service: Service
+    routes: Route[]
+}
+
+export type Route = {
+    path: string,
+    type: RouteType
+}
+
+export enum RouteType {
+    GET = "get",
+    POST = "post",
+    PUT = "put",
+    DELETE = "delete"
+}
+
+/**
+ * register all controllers and their services routes to the Express app
+ * @param express
+ * @param controllers
+ */
+export function initControllers(express: Express, controllers: Controller[]): void {
+    controllers.forEach(controller => {
+            controller.routes.forEach(route => {
+                    express[route.type.toString()](`${controller.prefix}/${route.path}`, (
+                            req: Express.Request, res: Express.Response
+                        ) => {
+                            // find request listener by route path and type
+                            const requestListener: RequestListener = controller.service.listeners.find(
+                                listener => listener.route.path === route.path && listener.route.type === route.type
+                            );
+
+                            // check if listener was found
+                            if (!requestListener) {
+                                res.status(404).send(`Service wasn't registered for route ${controller.prefix}/${route.path}`);
+                                return;
+                            }
+                            const responseData: ServiceResponseData = requestListener.execute(req.body);
+                            res.status(responseData.status).json({
+                                ...responseData.data,
+                                message: responseData.message
+                            })
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
---
+++ Service.ts
@@ -0,0 +1,34 @@
+import {HttpStatus} from "http-status-ts";
+import {Route, RouteType} from "./Controller";
+
+export type Service = {
+    listeners: RequestListener[]
+}
+
+export type RequestListener = {
+    route: Route
+    execute: (data: any) => ServiceResponseData
+}
+
+export type ServiceResponseData = {
+    success: boolean,
+    message: string,
+    status: HttpStatus,
+    data: any
+}
+
+/**
+ * create request listener data object to saving code lines
+ * @param routeType
+ * @param routePath
+ * @param execute
+ */
+export function generateListener(routeType: RouteType, routePath: string, execute: (data: any) => ServiceResponseData): RequestListener {
+    return {
+        route: {
+            type: routeType,
+            path: routePath
+        },
+        execute
+    }
+}
