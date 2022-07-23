26f4b3ffaf8d60a35b54ea28c2d66ab6ab023640
peu77 <emilebert@icloud.com>
2022-07-23 13:07:58 +0200

    test github workflow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
-import {Controller, initControllers, Route, RouteType} from "./Controller";
+import {
+    Controller,
+    initControllers,
+    Route,
+    RouteType,
+    DependencyOption,
+    DependencyResponse,
+    DependencyImpl,
+} from "./Controller";
 import {Service, ServiceResponseData, generateListener, RequestListener} from "./Service";
+import {GuardFunction} from "./Guard";
+import {DependencyOptions} from "./DependencyOptions";
 
-export {Controller, Service, initControllers, Route, RouteType, ServiceResponseData, generateListener, RequestListener};
+export {
+    Controller,
+    Service,
+    initControllers,
+    Route,
+    RouteType,
+    ServiceResponseData,
+    generateListener,
+    RequestListener,
+    DependencyResponse,
+    DependencyOption,
+    DependencyImpl,
+    GuardFunction,
+    DependencyOptions
+};
